3ec8f0ad6787ef9a6cf5f91753ce032a43605950
aravind.ln <aravind.ln@gavstech.com>
2023-10-27 21:27:57 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:'<div style="color:magneta"><b>Welcome to Angular</b><u>Intro to Angular</u></div>',
+  styles:['b{color:cyan}','div{color:yellow}']
 })
 export class AppComponent {
-  title = 'firstangular';
+  title = 'First Website in Angular';
+  name:string='Aravind LN';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule
+    ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,RegistrationComponent,AboutusComponent,ContactusComponent]
 })
 export class AppModule { }
